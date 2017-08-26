943a700fe7a53c84b43ce3ff466d0f455f55ddbc
Kuldeep Sharma <kul123sharma@gmail.com>
2017-08-26 18:48:21 +0530

    26.8.17.2

--- ex.cpp
+++ ex.cpp
@@ -1,34 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
-int Egg(int n,int f){
-	int dp[n+1][f+1];
-	for(int j=0;j<f+1;j++)	dp[1][j]=j;
-	for(int i=0;i<n+1;i++)	dp[i][0]=0;
-	for(int i=2;i<n+1;i++)
-		for(int j=1;j<f+1;j++){
-			dp[i][j]=INT_MAX;
-			for(int k=1;k<=j;k++)	dp[i][j]=min(dp[i][j],1+max(dp[i-1][k-1],dp[i][j-k]));
-		}
-	return dp[n][f];
-}
 int main(){
-	freopen("input.txt","r",stdin);	//freopen("output.txt","w",stdout);
-	int t;	cin>>t;
-	while(t--){
-		int n,f; cin>>n>>f;
-		cout<<Egg(n,f)<<endl;
-	}
-	return 0;
+	freopen("input.txt","r",stdin);	freopen("output.txt","w",stdout);
+	long long n,m;	cin>>n>>m;
+	long long a[n][m];
+	for(long long i=0;i<n;i++)
+		for(long long j=0;j<m;j++)
+			cin>>a[i][j];
+    long long u=0;
+    for(long long i=0;i<n;i++)
+    	for(long long j=0;j<m;j++){
+    		if(i==0)	a[i][j]+=u;
+    		a[i][j]+=a[i][j-1];
+    		if(j==m-1)	u=a[i][j];
+    	}
+    long long t;  cin>>t;
+    while(t--){
+    	long long x,y;  cin>>x>>y;
+        cout<<a[x-1][y-1]<<endl;
+    }
+    return 0;
 }
-/*
-int Egg(int n,int f){
-	if(f==0 || f==1)	return f;
-	if(n==1)	return f;
-	int r,u=INT_MAX;
-	for(int k=1;k<=f;k++){
-		r=max(Egg(n-1,k-1),Egg(n,f-k));
-		if(u>r)	u=r;
-	}
-	return u+1;
-}
-*/
+
